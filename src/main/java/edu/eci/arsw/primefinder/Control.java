9c8dbc8ba2971b24dcaf08905e73a79c69834a8c
unknown <mateo.forero-f@labinfo.is.escuelaing.edu.co>
2025-02-01 12:43:10 -0500

    feat: initial

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -5,40 +5,56 @@
  */
 package edu.eci.arsw.primefinder;
 
+import java.util.Scanner;
+
 /**
  *
  */
 public class Control extends Thread {
-    
+
     private final static int NTHREADS = 3;
-    private final static int MAXVALUE = 30000000;
-    private final static int TMILISECONDS = 5000;
+    private final static int MAXVALUE = 4000;
+    private final static int TMILISECONDS = 10;
 
     private final int NDATA = MAXVALUE / NTHREADS;
 
     private PrimeFinderThread pft[];
-    
+    private long initialMS;
+
     private Control() {
+        
         super();
-        this.pft = new  PrimeFinderThread[NTHREADS];
+        initialMS = System.nanoTime(); 
+        this.pft = new PrimeFinderThread[NTHREADS];
 
         int i;
-        for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
+        for (i = 0; i < NTHREADS - 1; i++) {
+            PrimeFinderThread elem = new PrimeFinderThread(i * NDATA, (i + 1) * NDATA);
             pft[i] = elem;
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1);
     }
-    
+
     public static Control newControl() {
         return new Control();
     }
 
     @Override
     public void run() {
-        for(int i = 0;i < NTHREADS;i++ ) {
+        for (int i = 0; i < NTHREADS; i++) {
             pft[i].start();
         }
+        try {
+            boolean notFinished = true;
+            while (notFinished) {
+                long actualSEC = (System.nanoTime()-initialMS)/1000000;
+                if (actualSEC % 1000 == 0){
+                    System.out.println("Time:" + actualSEC);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
+
 }
