dc2577eefb21ae19a464527ba4064725097ea66a
JaiderVargas05 <Jaider.vargas-n@mail.escuelaing.edu.co>
2025-02-02 11:06:50 -0500

    Practica 1

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -13,8 +13,8 @@ import java.util.Scanner;
 public class Control extends Thread {
 
     private final static int NTHREADS = 3;
-    private final static int MAXVALUE = 4000;
-    private final static int TMILISECONDS = 10;
+    private final static int MAXVALUE = 40000000;
+    private final static int TMILISECONDS = 3000;
 
     private final int NDATA = MAXVALUE / NTHREADS;
 
@@ -41,16 +41,30 @@ public class Control extends Thread {
 
     @Override
     public void run() {
+        Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < NTHREADS; i++) {
             pft[i].start();
         }
         try {
-            boolean notFinished = true;
-            while (notFinished) {
-                long actualSEC = (System.nanoTime()-initialMS)/1000000;
-                if (actualSEC % 1000 == 0){
-                    System.out.println("Time:" + actualSEC);
+            boolean isAllDead = false;
+            while(!isAllDead){
+                isAllDead = true;
+                for (int i = 0; i < NTHREADS; i++) {
+                    if(pft[i].isAlive()){
+                        isAllDead=false;
+                        if(pft[i].isPaused())pft[i].resumeThread();
+                    }
                 }
+                if(isAllDead)break;
+                Thread.sleep(TMILISECONDS);
+                int totalPrimes = 0;
+                for (int i = 0; i < NTHREADS; i++) {
+                    pft[i].pauseThread();
+                    totalPrimes += pft[i].getActualPrimes();
+                }
+                System.out.println("Total primes calculated: "+totalPrimes);
+                System.out.println("Press enter to continue");
+                scanner.nextLine();
             }
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -8,7 +8,8 @@ public class PrimeFinderThread extends Thread {
     int a, b;
 
     private List<Integer> primes;
-
+    private final Object lock = new Object();
+    private boolean isPaused = false;
     public PrimeFinderThread(int a, int b) {
         super();
         this.primes = new LinkedList<>();
@@ -19,11 +20,34 @@ public class PrimeFinderThread extends Thread {
     @Override
     public void run() {
         for (int i = a; i < b; i++) {
+            synchronized (lock){
+                while(isPaused){
+                    try{
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+            }
             if (isPrime(i)) {
                 primes.add(i);
                 System.out.println(i);
             }
+
         }
+    }
+    public void pauseThread(){
+        synchronized (lock){
+            isPaused = true;
+        }
+    }
+    public void resumeThread(){
+        synchronized (lock){
+            isPaused=false;
+            lock.notify();
+        }
+
     }
 
     boolean isPrime(int n) {
@@ -46,5 +70,9 @@ public class PrimeFinderThread extends Thread {
     public int getActualPrimes(){
         return primes.size();
     }
-
+    public  boolean isPaused(){
+        synchronized (lock){
+            return isPaused;
+        }
+    }
 }
